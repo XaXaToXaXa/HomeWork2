bb62429a752960d2ac5f1ba5eb6b9d92e7cd9ede
XaXaToXaXa <XaXaToXaXa@gmail.com>
2022-12-21 16:30:45 +0300

    Add Second  HomeWork

---
+++ Task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32 (Console.ReadLine());
+if (num < -99 || num > 99) 
+{
+    if (num > 0) Console.WriteLine($"Третья цифра = {num.ToString()[2]}");
+    else Console.WriteLine($"Третья цифра = {num.ToString()[3]}");
+}
+else Console.WriteLine("Нет третьей цифры");
