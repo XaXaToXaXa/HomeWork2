de0bd3b5512ad0447dd1ee1f6f3bfa907e757376
XaXaToXaXa <XaXaToXaXa@gmail.com>
2022-12-21 16:30:31 +0300

    Add First HomeWork

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+int a = (num%100)/10;
+Console.WriteLine(a);
