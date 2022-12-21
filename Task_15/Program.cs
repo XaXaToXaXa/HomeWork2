b87f03bb1a60834af31cb0b3ca4fabc5c121aea3
XaXaToXaXa <XaXaToXaXa@gmail.com>
2022-12-21 16:31:07 +0300

    Add Third HomeWork

---
+++ Task_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет, является ли
+// этот день выходным.
+
+//Simple way
+// Console.WriteLine ("Введите число");
+// int num = Convert.ToInt32 (Console.ReadLine());
+// if (num == 6 || num == 7) 
+// {
+//     Console.WriteLine ("Weekeend!!!");
+// }
+// else 
+// {
+// Console.WriteLine("Boringday");
+// }
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32 (Console.ReadLine());
+if ( num % 7  == 0 || (num + 1) % 7 == 0)  
+{
+    Console.WriteLine ("Weekeend!!!");
+}
+else 
+{
+Console.WriteLine("Boringday");
+}
